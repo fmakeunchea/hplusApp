53b8f1fff543c2e5da46bc38307820fa51f9de5c
Fifi Makeunchea <fmakeunch@gmail.com>
2020-07-29 20:31:43 -0400

    The exception handling works fine

--- src/main/java/com/test/hplus/controllers/LoginController.java
+++ src/main/java/com/test/hplus/controllers/LoginController.java
@@ -3,8 +3,10 @@ package com.test.hplus.controllers;
 import com.test.hplus.Repository.UserRepository;
 import com.test.hplus.beans.Login;
 import com.test.hplus.beans.User;
+import com.test.hplus.exception.ApplicationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,6 +18,13 @@ public class LoginController {
     @PostMapping("/login")
     public String login(@ModelAttribute("login")Login login){
         User user = userRepository.searchByName(login.getUsername());
+        if (user ==null){
+            throw new ApplicationException("User not found");        }
         return "search";
+    }
+    @ExceptionHandler(ApplicationException.class)
+    public String handleException(){
+        System.out.println("In exception handler of login controller");
+        return "error";
     }
 }
